f1bd5b4a24721fa7650b0856dc86585eb18eefe2
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2022-01-23 00:15:42 +0300

    Delete credit-service/src/main/java/com/example/creditservice directory

--- credit-service/src/main/java/com/example/creditservice/enums/CreditResultResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.creditservice.enums;
-
-public enum CreditResultResponse {
-    CONFIRMATION,
-    REJECTION
-}
--- credit-service/src/main/java/com/example/creditservice/repository/CreditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.creditservice.repository;
-
-import com.example.creditservice.model.Credit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreditRepository extends JpaRepository<Credit,Long> {
-}
